4e022430bb9627c424bbd72e340d01f6e801dfd3
paulo tipay <paulo@kodego.ph>
2022-09-03 09:49:19 +0800

    initial commit for KotlinProgram2.kt

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,26 @@
+fun main(){
+    var string1 : String = "this is my String"
+    var string2 : String = "this is my String too!"
+    var string3 : String = "this is my String"
+    //string character
+    println(string1[11])
+    //string length
+    println(string1.length)
+    println(string2.length)
+    //string equality
+    println(string1 == string3)
+    //string uppercase/lowercase
+    println(string2.uppercase())
+    //string uppercase/lowercase
+    println(string2.lowercase())
+    //startswith
+    println(string2.startsWith("tha"))
+    //endsswith
+    println(string2.endsWith("too!"))
+    //concatenate
+    println(string1+" "+string2)
+    //substring
+    println(string1.substring(6,10))
+    //replace
+    println(string1.replace("this","that"))
+}
